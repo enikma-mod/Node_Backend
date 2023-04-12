4ef35f1c8b967de5467644b630dab0fae4e1532b
Karabo Modika <80250498+enikma-mod@users.noreply.github.com>
2023-04-12 14:20:03 +0200

    backend node

---
+++ app/config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // url: "mongodb+srv://modika:Q8EeprVEdmDPcDdH@cluster0.chexzjs.mongodb.net/test"
+    // url: "mongodb+srv://modika:nLOLs4GFB0IpH3MU@cluster0.ka3fbdy.mongodb.net/test"
+    url: "mongodb+srv://modika:nLOLs4GFB0IpH3MU@cluster0.ka3fbdy.mongodb.net/?retryWrites=true&w=majority"
+  };
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./app/models");
+// require("./app/routes/tutorial.routes")(app);
+const app = express();
+
+const tutorials = require("./app/controllers/tutorial.controllers");
+  
+var router = require("express").Router();
+
+
+var corsOptions = {
+  origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+
+// set port, listen for requests
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+
+  
+    // // Create a new Tutorial
+    // router.post("/api/tutorials", tutorials.create);
+  
+    // // Retrieve all Tutorials
+    // router.get("/api/tutorials", tutorials.findAll);
+  
+    // // Retrieve all published Tutorials
+    // router.get("/published", tutorials.findAllPublished);
+  
+    // // Retrieve a single Tutorial with id
+    // router.get("/:id", tutorials.findOne);
+  
+    // // Update a Tutorial with id
+    // router.put("/:id", tutorials.update);
+  
+    // // Delete a Tutorial with id
+    // router.delete("/:id", tutorials.delete);
+  
+    // // Delete all Tutorials
+    // router.delete("/api/tutorials", tutorials.deleteAll);
+  
+    // app.use('/api/tutorials', router);
+
+
